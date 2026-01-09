06ad516eecf0281a62ae1f6fe23f3e0dd5042098
bosseschum <bosse.schum@gmail.com>
2026-01-09 14:21:49 +0100

    Size prompt added

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 const page = document.querySelector(".container");
 const body = document.querySelector("body");
 
-const grid = new Array(16);
-for (let i = 0; i < 16; i++) {
-  grid[i] = new Array(16);
+const newGridButton = document.createElement("button");
+newGridButton.textContent = "New Grid";
+newGridButton.addEventListener("click", () => {
+  let input = prompt("How many squares per side?");
+  createGrid(input);
+});
 
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    div.style.width = "25px";
-    div.style.height = "25px";
-    div.style.border = "1px solid black";
-    div.style.boxSizing = "borderBox";
-    div.addEventListener("mouseover", (event) => {
-      div.style.backgroundColor = "black";
-    });
-    page.appendChild(div);
+body.appendChild(newGridButton);
+
+function createGrid(x) {
+  page.textContent = "";
+  page.style.width = `${x * 25}px`;
+
+  const grid = new Array(x);
+  for (let i = 0; i < x; i++) {
+    grid[i] = new Array(x);
+
+    for (let j = 0; j < x; j++) {
+      let div = document.createElement("div");
+      div.style.width = "25px";
+      div.style.height = "25px";
+      div.style.border = "1px solid black";
+      div.style.boxSizing = "border-box";
+      div.addEventListener("mouseover", (event) => {
+        div.style.backgroundColor = "black";
+      });
+      page.appendChild(div);
+    }
   }
 }
 
 page.style.display = "flex";
 page.style.flexWrap = "wrap";
-page.style.width = "450px";
